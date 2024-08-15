4f36dc7e56192a91f8adfee02dd901f0f20371c4
lPHlRalph <ralph.castanares@gmail.com>
2024-08-16 03:43:55 +0800

    first commit

---
+++ PalindromeChecker/Controllers/PalindromeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalindromeChecker.Controllers
+{
+    public class PalindromeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PalindromeCheck(int number)
+        {
+            if (number < 0) return BadRequest("Please input positive numbers only.");
+
+            var originalNumber = number.ToString();
+            var reversedNumber = new string(originalNumber.Reverse().ToArray());
+            bool isPalindrome = originalNumber == reversedNumber;
+
+            return Json(new {Number  = number, IsPalindrome = isPalindrome});
+            
+        }
+    }
+}
---
+++ PalindromeChecker/wwwroot/Scripts/Palindrome.js
@@ -0,0 +1,14 @@
+﻿document.getElementById('checkForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    var number = document.getElementById('txtNumberInput').value;
+    var output;
+    try {
+        var response = await fetch(`/Palindrome/PalindromeCheck?number=${number}`);
+        var result = await response.json();
+        output = result.isPalindrome ? `${result.number} is a palindrome.` : `${result.number} is not a palindrome.`;
+    } catch (err) {
+        output = "Please input positive numbers only.";
+    }
+    
+    document.getElementById('result').innerText = output;
+});
